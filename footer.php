a760833e6ddea23fe3946694ce37572f4e41eed3
Justin Tadlock <justin@justintadlock.com>
2014-05-20 16:18:55 -0500

    Move `</div><!-- .wrap -->` to correct location.

--- footer.php
+++ footer.php
@@ -4,24 +4,24 @@
 
 			<?php hybrid_get_sidebar( 'subsidiary' ); // Loads the sidebar/subsidiary.php template. ?>
 
+			<footer <?php hybrid_attr( 'footer' ); ?>>
+
+				<?php hybrid_get_menu( 'social' ); // Loads the menu/social.php template. ?>
+
+				<div class="footer-content">
+					<p class="copyright">
+						<?php printf(
+							/* Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link. */
+							__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'ravel' ), 
+							date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
+						); ?>
+					</p><!-- .copyright -->
+
+				</div><!-- .footer-content -->
+
+			</footer><!-- #footer -->
+
 		</div><!-- .wrap -->
-
-		<footer <?php hybrid_attr( 'footer' ); ?>>
-
-			<?php hybrid_get_menu( 'social' ); // Loads the menu/social.php template. ?>
-
-			<div class="footer-content">
-				<p class="copyright">
-					<?php printf(
-						/* Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link. */
-						__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'ravel' ), 
-						date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
-					); ?>
-				</p><!-- .copyright -->
-
-			</div><!-- .footer-content -->
-
-		</footer><!-- #footer -->
 
 	</div><!-- #container -->
 
