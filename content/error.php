d2c5dffc5b4c8013cb89632ad762b5c962ddbd7b
Justin Tadlock <justin@justintadlock.com>
2014-05-20 22:02:22 -0500

    Add content/error.php template.

---
+++ content/error.php
@@ -0,0 +1,11 @@
+<article class="entry error">
+
+	<header class="entry-header">
+		<h1 class="entry-title"><?php _e( 'Error', 'ravel' ); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div <?php hybrid_attr( 'entry-content' ); ?>>
+		<?php get_search_form(); ?>
+	</div><!-- .entry-content -->
+
+</article><!-- .entry -->
