5aded879d7097fc510c8ccb955c8c997ff1d165a
Justin Tadlock <justin@justintadlock.com>
2014-05-20 19:18:51 -0500

    Add content/gallery.php template.

---
+++ content/gallery.php
@@ -0,0 +1,59 @@
+<article <?php hybrid_attr( 'post' ); ?>>
+
+	<?php if ( is_singular( get_post_type() ) ) : // If viewing a single post. ?>
+
+		<header class="entry-header">
+
+			<div class="entry-byline">
+				<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
+				<?php hybrid_post_author( array( 'text' => __( 'By %s', 'ravel' ) ) ); ?>
+				<?php comments_popup_link( __( '0 Comments', 'ravel' ), __( '1 Comment', 'ravel' ), __( '% Comments', 'ravel' ), 'comments-link', '' ); ?>
+				<?php edit_post_link(); ?>
+			</div><!-- .entry-byline -->
+
+			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
+
+		</header><!-- .entry-header -->
+
+		<div <?php hybrid_attr( 'entry-content' ); ?>>
+			<?php the_content(); ?>
+			<?php wp_link_pages(); ?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<a class="entry-permalink" href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php _e( 'Permalink', 'ravel' ); ?></a>
+			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'sep' => ' ' ) ); ?>
+			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'sep' => ' ' ) ); ?>
+		</footer><!-- .entry-footer -->
+
+	<?php else : // If not viewing a single post. ?>
+
+		<header class="entry-header">
+
+			<div class="entry-byline">
+				<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
+				<?php hybrid_post_author( array( 'text' => __( 'By %s', 'ravel' ) ) ); ?>
+				<?php comments_popup_link( __( '0 Comments', 'ravel' ), __( '1 Comment', 'ravel' ), __( '% Comments', 'ravel' ), 'comments-link', '' ); ?>
+				<?php edit_post_link(); ?>
+			</div><!-- .entry-byline -->
+
+			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
+
+		</header><!-- .entry-header -->
+
+		<?php $count = hybrid_get_gallery_item_count(); ?>
+		<?php get_the_image( array( 'size' => 'ravel-medium', 'before' => '<div class="featured-media">', 'after' => '<span class="gallery-items-count">' . sprintf( _n( '%s item', '%s items', $count, 'ravel' ), $count ) . '</span></div>' ) ); ?>
+
+		<div <?php hybrid_attr( 'entry-summary' ); ?>>
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+
+		<footer class="entry-footer">
+			<a class="entry-permalink" href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php _e( 'Permalink', 'ravel' ); ?></a>
+			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'sep' => ' ' ) ); ?>
+			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'sep' => ' ' ) ); ?>
+		</footer><!-- .entry-footer -->
+
+	<?php endif; // End single post check. ?>
+
+</article><!-- .entry -->
