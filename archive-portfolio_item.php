c832d78a41b8a46305e155778d87a02d977f62be
Justin Tadlock <justin@justintadlock.com>
2014-05-20 22:58:01 -0500

    Add archive-portfolio_item.php and taxonomy-portfolio.php templates.  These templates duplicate too much code.  Need to consolidate them.

---
+++ archive-portfolio_item.php
@@ -0,0 +1,35 @@
+<?php get_header(); // Loads the header.php template. ?>
+
+<main <?php hybrid_attr( 'content' ); ?>>
+
+	<?php locate_template( array( 'misc/loop-meta.php' ), true ); // Loads the misc/loop-meta.php template. ?>
+
+	<?php if ( have_posts() ) : // Checks if any posts were found. ?>
+
+		<ul class="loop-entries-gallery">
+
+			<?php while ( have_posts() ) : // Begins the loop through found posts. ?>
+
+				<?php the_post(); // Loads the post data. ?>
+
+				<li>
+					<figure>
+						<?php get_the_image( array( 'size' => 'ravel-medium', 'scan' => true, 'order' => array( 'scan', 'featured', 'attachment' ) ) ); ?>
+					</figure>
+				</li>
+
+			<?php endwhile; // End found posts loop. ?>
+
+		</ul>
+
+		<?php locate_template( array( 'misc/loop-nav.php' ), true ); // Loads the misc/loop-nav.php template. ?>
+
+	<?php else : // If no posts were found. ?>
+
+		<?php locate_template( array( 'content/error.php' ), true ); // Loads the content/error.php template. ?>
+
+	<?php endif; // End check for posts. ?>
+
+</main><!-- #content -->
+
+<?php get_footer(); // Loads the footer.php template. ?>
