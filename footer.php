ee93ad1a2c8159f0dbd0b843f6d88b3d0a00eae4
Justin Tadlock <justin@justintadlock.com>
2014-05-20 22:06:16 -0500

    Add Primary sidebar.

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
-				<?php //hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template. ?>
+				<?php hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template. ?>
 
 			</div><!-- #main -->
-
-			<?php hybrid_get_sidebar( 'subsidiary' ); // Loads the sidebar/subsidiary.php template. ?>
 
 			<footer <?php hybrid_attr( 'footer' ); ?>>
 
