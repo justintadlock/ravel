fc8e65811b1d59a9d04d72b8ad2a2759553849cc
Justin Tadlock <justin@justintadlock.com>
2014-05-21 18:37:43 -0500

    Add comment edit link.

--- comment/comment.php
+++ comment/comment.php
@@ -6,8 +6,8 @@
 			<cite <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite><br />
 			<time <?php hybrid_attr( 'comment-published' ); ?>>
 				<a <?php hybrid_attr( 'comment-permalink' ); ?>><?php printf( __( '%s ago', 'stargazer' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?></a>
-			</time>
-			<?php //edit_comment_link(); ?>
+			</time> 
+			<?php edit_comment_link(); ?>
 		</header><!-- .comment-meta -->
 
 		<div <?php hybrid_attr( 'comment-content' ); ?>>
