9b57a709bc7b0ef3beffcffbf3bc7e54183dc67e
Justin Tadlock <justin@justintadlock.com>
2014-05-20 19:29:15 -0500

    Add content/page.php template.

---
+++ content/page.php
@@ -0,0 +1,32 @@
+<article <?php hybrid_attr( 'post' ); ?>>
+
+	<?php if ( is_page() ) : // If viewing a single page. ?>
+
+		<header class="entry-header">
+
+			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
+
+		</header><!-- .entry-header -->
+
+		<div <?php hybrid_attr( 'entry-content' ); ?>>
+			<?php the_content(); ?>
+			<?php wp_link_pages(); ?>
+		</div><!-- .entry-content -->
+
+	<?php else : // If not viewing a single page. ?>
+
+		<header class="entry-header">
+
+			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
+
+			<?php get_the_image( array( 'size' => 'ravel-medium', 'before' => '<div class="featured-media">', 'after' => '</div>' ) ); ?>
+
+		</header><!-- .entry-header -->
+
+		<div <?php hybrid_attr( 'entry-summary' ); ?>>
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+
+	<?php endif; // End single page check. ?>
+
+</article><!-- .entry -->
