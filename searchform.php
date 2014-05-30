5aac681e68cdba4b4f826b16c2469d18599dde7f
Justin Tadlock <justin@justintadlock.com>
2014-05-29 19:53:38 -0500

    HTML fix for search form.

--- searchform.php
+++ searchform.php
@@ -1,8 +1,7 @@
 <form method="get" class="search-form" action="<?php echo trailingslashit( home_url() ); ?>">
 	<div>
-		<label><span class="screen-reader-text"><?php _e( 'Search', 'ravel' ); ?></span>
-			<input class="search-text" type="text" name="s" placeholder="<?php esc_attr_e( 'Search for...', 'ravel' ); ?>" />
-		</label>
+		<label><span class="screen-reader-text"><?php _e( 'Search', 'ravel' ); ?></span></label>
+		<input class="search-text" type="text" name="s" placeholder="<?php esc_attr_e( 'Search for...', 'ravel' ); ?>" />
 		<input class="search-submit button" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'ravel' ); ?>" />
 	</div>
 </form><!-- .search-form -->
