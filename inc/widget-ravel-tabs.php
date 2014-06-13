e14b45ae7f0fe745fdcac4ab026262a9d54de339
Justin Tadlock <justin@justintadlock.com>
2014-06-12 22:46:43 -0500

    Cleaned up the tabs widget and added new options in (see readme.md notes).  Possible @todo: Add in a "past X months" setting for the popular tab or make a definite decision about how far into the past to go (currently set at past 6 months).

--- inc/widget-ravel-tabs.php
+++ inc/widget-ravel-tabs.php
@@ -42,7 +42,11 @@ class Ravel_Widget_Tabs extends WP_Widget {
 
 		/* Set up the defaults. */
 		$this->defaults = array(
-			'title' => esc_attr__( 'Tabs', 'ravel' ),
+			'title'                   => '', // No default title.
+			'recent_posts_number'     => 3,
+			'popular_posts_number'    => 3,
+			'recent_comments_number'  => 3,
+			'tag_cloud_number'        => 45,
 		);
 	}
 
@@ -59,6 +63,24 @@ class Ravel_Widget_Tabs extends WP_Widget {
 
 		$args = wp_parse_args( $instance, $this->defaults );
 
+		/* Set up tabs. */
+		$tabs = array();
+
+		if ( 0 < $args['recent_posts_number'] )
+			$tabs['recent'] = __( 'Recent Posts', 'ravel' );
+
+		if ( 0 < $args['popular_posts_number'] )
+			$tabs['popular'] = __( 'Popular Posts', 'ravel' );
+
+		if ( 0 < $args['recent_comments_number'] )
+			$tabs['comments'] = __( 'Recent Comments', 'ravel' );
+
+		if ( 0 < $args['tag_cloud_number'] )
+			$tabs['tags'] = __( 'Tags', 'ravel' );
+
+		if ( empty( $tabs ) )
+			return;
+
 		/* Output the sidebar's $before_widget wrapper. */
 		echo $sidebar['before_widget'];
 
@@ -66,12 +88,6 @@ class Ravel_Widget_Tabs extends WP_Widget {
 		if ( !empty( $args['title'] ) )
 			echo $sidebar['before_title'] . apply_filters( 'widget_title', $args['title'], $instance, $this->id_base ) . $sidebar['after_title'];
 
-			$tabs = array(
-				'recent'   => __( 'Recent Posts',    'ravel' ),
-				'popular'  => __( 'Popular Posts',   'ravel' ),
-				'comments' => __( 'Recent Comments', 'ravel' ),
-				'tags'     => __( 'Tags',            'ravel' )
-			);
 		?>
 
 		<ul class="tabs-nav group tab-count-4">
@@ -82,112 +98,137 @@ class Ravel_Widget_Tabs extends WP_Widget {
 
 		<div class="tabs-container">
 
-			<?php $loop = new WP_Query( 
-				array( 
-					'posts_per_page'      => 3, 
-					'ignore_sticky_posts' => true 
-				) 
-			); ?>
+			<?php if ( 0 < $args['recent_posts_number'] ) : ?>
 
-			<ul id="<?php echo esc_attr( $this->id_base ); ?>-recent" class="tabs-panel group thumbs-enabled">
+				<?php $loop = new WP_Query( 
+					array( 
+						'posts_per_page'      => $args['recent_posts_number'], 
+						'ignore_sticky_posts' => true 
+					) 
+				); ?>
 
-				<?php while ( $loop->have_posts() ) : ?>
+				<ul id="<?php echo esc_attr( $this->id_base ); ?>-recent" class="tabs-panel group thumbs-enabled">
 
-					<?php $loop->the_post(); ?>
+					<?php while ( $loop->have_posts() ) : ?>
 
-					<li>
-						<?php get_the_image( array( 'size' => 'post-thumbnail', 'before' => '<div class="tab-thumbnail">', 'after' => '</div>' ) ); ?>
-
-						<div class="tab-content">
-
-							<div class="tab-category">
-								<?php hybrid_post_format_link(); ?>
-							</div><!-- .tab-category -->
-
-							<?php the_title( '<div class="tab-title"><a href="' . get_permalink() . '">', '</a></div>' ); ?>
-
-							<div class="tab-date"><?php echo get_the_date(); ?></div>
-
-						</div><!-- .tab-content -->
-					</li>
-
-				<?php endwhile; ?>
-			</ul>
-
-			<?php $loop = new WP_Query( 
-				array( 
-					'posts_per_page'      => 3, 
-					'orderby'             => 'comment_count', 
-					'ignore_sticky_posts' => true,
-					'date_query'          => array(
-						array(
-							'after' => '1 month ago'
-						)
-					)
-				) 
-			); ?>
-
-			<ul id="<?php echo esc_attr( $this->id_base ); ?>-popular" class="tabs-panel thumbs-enabled">
-
-				<?php while ( $loop->have_posts() ) : ?>
-
-					<?php $loop->the_post(); ?>
-
-					<li>
-						<?php get_the_image( array( 'size' => 'post-thumbnail', 'before' => '<div class="tab-thumbnail">', 'after' => '</div>' ) ); ?>
-
-						<div class="tab-content">
-
-							<div class="tab-category">
-								<?php hybrid_post_format_link(); ?>
-							</div><!-- .tab-category -->
-
-							<?php the_title( '<div class="tab-title"><a href="' . get_permalink() . '">', '</a></div>' ); ?>
-
-							<div class="tab-date"><?php echo get_the_date(); ?></div>
-
-						</div><!-- .tab-content -->
-					</li>
-
-				<?php endwhile; ?>
-			</ul>
-
-			<?php $comments = get_comments( array( 'number' => 3, 'status' => 'approve' ) ); ?>
-
-			<?php if ( !empty( $comments ) ) : ?>
-
-				<ul id="<?php echo esc_attr( $this->id_base ); ?>-comments" class="tabs-panel avatars-enabled">
-
-					<?php foreach ( $comments as $comment ) : ?>
+						<?php $loop->the_post(); ?>
 
 						<li>
-							<div class="tab-thumbnail avatar">
-								<?php printf( '<a href="%s">%s</a>', esc_url( get_comment_link( $comment->comment_ID ) ), get_avatar( $comment->comment_author_email, 96 ) ); ?>
-							</div>
+							<?php get_the_image( array( 'size' => 'post-thumbnail', 'before' => '<div class="tab-thumbnail">', 'after' => '</div>' ) ); ?>
 
 							<div class="tab-content">
-								<div class="tab-author">
-									<cite><?php comment_author( $comment->comment_ID ); ?></cite>
-								</div>
-								<div class="tab-comment">
-									<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-										<?php comment_excerpt( $comment->comment_ID ); ?>
-									</a>
-								</div>	
+
+								<div class="tab-category">
+									<?php hybrid_post_format_link(); ?>
+								</div><!-- .tab-category -->
+
+								<?php the_title( '<div class="tab-title"><a href="' . get_permalink() . '">', '</a></div>' ); ?>
+
+								<div class="tab-date"><?php echo get_the_date(); ?></div>
+
 							</div><!-- .tab-content -->
 						</li>
 
-					<?php endforeach; ?>
+					<?php endwhile; ?>
+				</ul>
 
-				</ul><!--.tabs-panel-->
+			<?php endif; // End check to display recent posts. ?>
 
-			<?php endif; ?>
+			<?php if ( 0 < $args['popular_posts_number'] ) : ?>
 
-			<ul id="<?php echo esc_attr( $this->id_base ); ?>-tags" class="tabs-panel">
-				<li>
-					<?php wp_tag_cloud(); ?>
-				</li>
-			</ul><!-- .tabs-panel -->
+				<?php $loop = new WP_Query( 
+					array( 
+						'posts_per_page'      => $args['popular_posts_number'], 
+						'orderby'             => 'comment_count', 
+						'ignore_sticky_posts' => true,
+						'date_query'          => array(
+							array(
+								'after' => '6 months ago'
+							)
+						)
+					) 
+				); ?>
+
+				<ul id="<?php echo esc_attr( $this->id_base ); ?>-popular" class="tabs-panel thumbs-enabled">
+
+					<?php while ( $loop->have_posts() ) : ?>
+
+						<?php $loop->the_post(); ?>
+
+						<li>
+							<?php get_the_image( array( 'size' => 'post-thumbnail', 'before' => '<div class="tab-thumbnail">', 'after' => '</div>' ) ); ?>
+
+							<div class="tab-content">
+
+								<div class="tab-category">
+									<?php hybrid_post_format_link(); ?>
+								</div><!-- .tab-category -->
+
+								<?php the_title( '<div class="tab-title"><a href="' . get_permalink() . '">', '</a></div>' ); ?>
+
+								<div class="tab-date"><?php echo get_the_date(); ?></div>
+
+							</div><!-- .tab-content -->
+						</li>
+
+					<?php endwhile; ?>
+				</ul>
+
+			<?php endif; // End popular posts check. ?>
+
+			<?php if ( 0 < $args['recent_comments_number'] ) : ?>
+
+				<?php $comments = get_comments(
+					array( 
+						'number' => $args['recent_comments_number'], 
+						'status' => 'approve'
+					) 
+				); ?>
+
+				<?php if ( !empty( $comments ) ) : ?>
+
+					<ul id="<?php echo esc_attr( $this->id_base ); ?>-comments" class="tabs-panel avatars-enabled">
+
+						<?php foreach ( $comments as $comment ) : ?>
+
+							<li>
+								<div class="tab-thumbnail avatar">
+									<?php printf( '<a href="%s">%s</a>', esc_url( get_comment_link( $comment->comment_ID ) ), get_avatar( $comment->comment_author_email, 96 ) ); ?>
+								</div>
+
+								<div class="tab-content">
+									<div class="tab-author">
+										<cite><?php comment_author( $comment->comment_ID ); ?></cite>
+									</div>
+									<div class="tab-comment">
+										<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+											<?php comment_excerpt( $comment->comment_ID ); ?>
+										</a>
+									</div>	
+								</div><!-- .tab-content -->
+							</li>
+
+						<?php endforeach; ?>
+
+					</ul><!--.tabs-panel-->
+
+				<?php endif; // End if have comments check. ?>
+
+			<?php endif; // End check for comments display. ?>
+
+			<?php if ( 0 < $args['tag_cloud_number'] ) : ?>
+
+				<ul id="<?php echo esc_attr( $this->id_base ); ?>-tags" class="tabs-panel">
+					<li>
+						<?php wp_tag_cloud(
+							array(
+								'number' => $args['tag_cloud_number'] 
+							)
+						); ?>
+					</li>
+				</ul><!-- .tabs-panel -->
+
+			<?php endif; // End check for display of tag cloud. ?>
 
 		</div><!-- .tabs-container -->
 		<?php
@@ -211,6 +252,12 @@ class Ravel_Widget_Tabs extends WP_Widget {
 		/* Strip tags. */
 		$instance['title'] = strip_tags( $new_instance['title'] );
 
+		/* Numbers. */
+		$instance['recent_posts_number']    = absint( $new_instance['recent_posts_number']    );
+		$instance['popular_posts_number']   = absint( $new_instance['popular_posts_number']   );
+		$instance['recent_comments_number'] = absint( $new_instance['recent_comments_number'] );
+		$instance['tag_cloud_number']       = absint( $new_instance['tag_cloud_number']       );
+
 		/* Return sanitized options. */
 		return $instance;
 	}
@@ -232,6 +279,23 @@ class Ravel_Widget_Tabs extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'ravel' ); ?></label>
 			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>" placeholder="<?php echo esc_attr( $this->defaults['title'] ); ?>" />
 		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'recent_posts_number' ); ?>"><?php _e( 'Number of recent posts to show?', 'ravel' ); ?></label>
+			<input type="number" class="smallfat code" size="5" min="0" id="<?php echo $this->get_field_id( 'recent_posts_number' ); ?>" name="<?php echo $this->get_field_name( 'recent_posts_number' ); ?>" value="<?php echo esc_attr( $instance['recent_posts_number'] ); ?>" placeholder="0" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'popular_posts_number' ); ?>"><?php _e( 'Number of popular posts to show?', 'ravel' ); ?></label>
+			<input type="number" class="smallfat code" size="5" min="0" id="<?php echo $this->get_field_id( 'popular_posts_number' ); ?>" name="<?php echo $this->get_field_name( 'popular_posts_number' ); ?>" value="<?php echo esc_attr( $instance['popular_posts_number'] ); ?>" placeholder="0" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'recent_comments_number' ); ?>"><?php _e( 'Number of comments to show?', 'ravel' ); ?></label>
+			<input type="number" class="smallfat code" size="5" min="0" id="<?php echo $this->get_field_id( 'recent_comments_number' ); ?>" name="<?php echo $this->get_field_name( 'recent_comments_number' ); ?>" value="<?php echo esc_attr( $instance['recent_comments_number'] ); ?>" placeholder="0" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'tag_cloud_number' ); ?>"><?php _e( 'Number of tags to show?', 'ravel' ); ?></label>
+			<input type="number" class="smallfat code" size="5" min="0" id="<?php echo $this->get_field_id( 'tag_cloud_number' ); ?>" name="<?php echo $this->get_field_name( 'tag_cloud_number' ); ?>" value="<?php echo esc_attr( $instance['tag_cloud_number'] ); ?>" placeholder="0" />
+		</p>
+		<div style="clear:both;">&nbsp;</div>
 	<?php
 	}
 }
--- readme.md
+++ readme.md
@@ -17,7 +17,8 @@ A work in progress...
 * `sidebar/primary.php now` checks for `if ( $layout !== 'layout-1c' )` instead of `get_theme_mod`
 
 ### Notes:
-* in `js/ravel.js`, `if ( jQuery( 'body' ).has( '.tabs-nav' ) ) {` seems to be irrelevant. If you change it to `.has( '.safdsafdsfas' )`, the code within the conditional still executes, but shouldn't.
+* <del>in `js/ravel.js`, `if ( jQuery( 'body' ).has( '.tabs-nav' ) ) {` seems to be irrelevant. If you change it to `.has( '.safdsafdsfas' )`, the code within the conditional still executes, but shouldn't.</del>
+	* <ins>The check isn't really needed.</ins>
 
 ## TODO's:
 
@@ -26,6 +27,7 @@ A work in progress...
 * <del>Filter `get_search_form()` output based on `searchform.php`</del>
 * Finish up tabs widget
 * Full RTL support? This is up to you. I'm a bit lazy to test for RTL. In `style.css`, there are some RTL CSS carried over from my previous themes.
+	* <ins>This can actually be quite involved and is best done from the ground up as the theme is being built. You'd do a better job with this since you wrote the CSS. For v.1.0, I say to wait on this.</ins>
 * Language .pot file.
 * Double check all files
 * Run theme check plugin
@@ -34,10 +36,18 @@ A work in progress...
 
 ### Tabs Widget:
 
-* This widget is used to display up to 4 groups of content: Recent Posts, Popular Posts, Comments, and Tags. You can also use it to display just `Recent Comments` or just `Popular Posts` -- I really like this part from the original widget.
+* <del>This widget is used to display up to 4 groups of content: Recent Posts, Popular Posts, Comments, and Tags. You can also use it to display just `Recent Comments` or just `Popular Posts` -- I really like this part from the original widget.</del>
+	* <ins>I decided to allow the user to set the number of posts, comments, or tags to "0".  In that case, the tab doesn't show.  It's not as intuitive, but it does cut out 4 options.</ins>
 * When more than one group of content are on display, the widget title is automatically hidden in favor of the tabs nav. Right now, the widget displays the title unless the title is empty.
 	* <ins>This is standard behavior for widgets.  If the user inputs a title, a title is expected.  If left empty, the title should not show.  Doing otherwise also can also mess with plugins that filter some of the widget-related hooks.</ins>
+	* <ins>Decided on not setting a default title for the widget. This way, the user must enter a title if they want it to show.</ins>
 * <del>Can you bring back the category link for the Recent Posts and Popular Posts entries or replace it with a post format archive link?</del>
-	* <ins>Used the post format link. That's easier to control since uses won't be putting 100 of those in.</ins>
-* For Recent Posts, Popular Posts, and Recent Comments, allow users to select number of entries to display.
+	* <ins>Used the post format link. That's easier to control since users won't be putting 100 of those in and saves some dev work getting the first cat.</ins>
+* <del>For Recent Posts, Popular Posts, and Recent Comments, allow users to select number of entries to display.</del>
+	* <ins>Can set the number of tags now too for consistency.</ins>
 * Allow users to set the order of tabs.
+	* <ins>This is ripe with problems (within my current skillset).  The only good way to do this is to allow the user to move this around with jQuery.  I haven't learned how to do this yet.</ins>
+
+Additional notes on tabs:  In general, we should keep the settings to a minimum.  If the theme is popular enough, users are going to want more options, more options, and more options (I've seen this with tab plugins before, which is why I built Whistles).  This is a bit of a slippery slope, so I want to just draw a line in the sand and say, "These are the options that are available, but if you want more control, check out this plugin over here."  But, it also allows us room to grow based on feedback if we want to add more stuff later.
+
+I'm going to play my developer card here and say that this is what I can do for v.1.0.
