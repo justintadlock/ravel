83c71c513054c2b1069e9862c9f3f679f967aba8
Justin Tadlock <justin@justintadlock.com>
2014-06-13 20:41:27 -0500

    Clean up portfolio_item.php and remove the duplicate calls to the_content().

--- content/portfolio_item.php
+++ content/portfolio_item.php
@@ -1,32 +1,24 @@
 <?php if ( is_singular( get_post_type() ) ) : // If viewing a single post. ?>
 
-	<?php
+	<?php if ( $attached_images = ravel_attached_images() ) : // If there are attached images. ?>
 
-		$attached_images = ravel_attached_images();
+		<?php echo $attached_images; ?>
 
-		if ( !empty( $attached_images ) ) { // If there are attached images
+	<?php else : // If no attached images. ?>
 
-			echo $attached_images;
+		<?php get_the_image( array( 'size' => 'ravel-large', 'attachment' => false, 'before' => '<div class="featured-media"><figure>', 'after' => '</figure></div>' ) ); ?>
 
-		} else {
-		
-			get_the_image( array( 'size' => 'ravel-large', 'attachment' => false, 'before' => '<div class="featured-media"><figure>', 'after' => '</figure></div>' ) );
-		
-		} // End check for attachments
-	?>
+	<?php endif; // End check for attachments. ?>
 
 	<article <?php hybrid_attr( 'post' ); ?>>
 
 		<header class="entry-header">
-
 			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
-
 		</header><!-- .entry-header -->
 
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php the_content(); ?>
 			<?php echo wpautop( ravel_get_portfolio_item_link() ); ?>
-			<?php the_content(); ?>
 			<?php edit_post_link(); ?>
 		</div><!-- .entry-content -->
 
