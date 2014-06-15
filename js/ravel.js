3d2dd63a659bb666b84aecffb5451663bcc12562
Justin Tadlock <justin@justintadlock.com>
2014-06-14 22:52:31 -0500

    Add embed-wrap JS for handling responsive videos.  Note: This is pulled from Stargazer and has some of the same issues, which should be fixed soonish.

--- js/ravel.js
+++ js/ravel.js
@@ -28,6 +28,26 @@ jQuery( document ).ready( function() {
 		);
 	}
 
+	/*
+	 * Video and other embeds.  Let's make them more responsive.	
+	 */
+
+	/* Overrides WP's <div> wrapper around videos, which mucks with flexible videos. */
+	jQuery( 'div[style*="max-width: 100%"] > video' ).parent().css( 'width', '100%' );
+
+	/* Responsive videos. */
+	/* blip.tv adds a second <embed> with "display: none".  We don't want to wrap that. */
+	jQuery( '.entry object, .entry embed, .entry iframe' ).not( 'embed[style*="display"], [src*="soundcloud.com"], [src*="amazon"], [name^="gform_"]' ).wrap( '<div class="embed-wrap" />' );
+
+	/* Removes the 'width' attribute from embedded videos and replaces it with a max-width. */
+	jQuery( '.embed-wrap object, .embed-wrap embed, .embed-wrap iframe' ).attr( 
+		'width',
+		function( index, value ) {
+			jQuery( this ).attr( 'style', 'max-width: ' + value + 'px;' );
+			jQuery( this ).removeAttr( 'width' );
+		}
+	);
+
 	/* Tabs. */
 	jQuery( '.widget_util_tabs .tabs-panel' ).hide();
 	jQuery( '.widget_util_tabs .tabs-panel:first-of-type' ).show();
